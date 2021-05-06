0ae75842ea6e6c7c2554e8bafba290d6531044a8
Denis <dineresc@gmail.com>
2021-05-06 02:25:31 -0300

    adding image

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -1,5 +1,39 @@
+import { Card, CardContent, Grid } from '@material-ui/core';
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+import signinImage from '../../static/images/signin.jpeg';
 
-export const SignInPage: Function = () => (
-  <h1>SignInPage</h1>
-);
+const useStyles = createUseStyles({
+  root: {
+    textAlign: 'center',
+  },
+  image: {
+    maxWidth: '500px',
+    maxHeight: '700px',
+  },
+});
+
+export const SignInPage: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Card>
+        <CardContent>
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <img className={classes.image} src={signinImage} alt="woman-signin" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card>
+                <CardContent>
+                  <h1>Jup</h1>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
