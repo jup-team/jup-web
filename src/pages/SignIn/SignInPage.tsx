ec6e4f9d748537f3b6d784a52b9eac0e7fa81703
Denis <dineresc@gmail.com>
2021-05-09 15:50:46 -0300

    creating image in signin

--- src/pages/SignIn/SignInPage.tsx
+++ src/pages/SignIn/SignInPage.tsx
@@ -1,4 +1,6 @@
-import { Card, CardContent, Grid } from '@material-ui/core';
+import {
+  Card, CardContent, Grid, Switch,
+} from '@material-ui/core';
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 import signinImage from '../../static/images/signin.jpeg';
@@ -8,13 +10,28 @@ const useStyles = createUseStyles({
     textAlign: 'center',
   },
   image: {
-    maxWidth: '500px',
-    maxHeight: '700px',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+  cardInputs: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '10px',
+    textAlign: 'center',
+    alignItems: 'center',
   },
 });
 
 export const SignInPage: React.FC = () => {
   const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    signinTypeForm: true,
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
   return (
     <div>
@@ -24,10 +41,17 @@ export const SignInPage: React.FC = () => {
             <Grid item xs={12} sm={6}>
               <img className={classes.image} src={signinImage} alt="woman-signin" />
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid className={classes.cardInputs} item xs={12} sm={6}>
               <Card>
                 <CardContent>
                   <h1>Jup</h1>
+                  <h3>Crie sua conta</h3>
+                  <Switch
+                    checked={state.signinTypeForm}
+                    onChange={handleChange}
+                    name="tipoForm"
+                    inputProps={{ 'aria-label': 'secondary checkbox' }}
+                  />
                 </CardContent>
               </Card>
             </Grid>
